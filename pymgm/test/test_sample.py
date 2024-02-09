dbaf4b622f31cd1c3ddb9bea74f55fe5b22a25c7
paulvxx <psv51037@gmail.com>
2024-02-08 22:42:25 -0700

    added test file

--- pymgm/test/test_sample.py
+++ pymgm/test/test_sample.py
@@ -1,8 +1,8 @@
 import pytest
 from src import sample
 
-assert sample.add_punct("Jump", "exclaimation")=="Jump!"
 
-assert sample.add_punct("Did he go yet", "question")=="Did he go yet?"
-
-assert sample.add_punct("I saw the cat walk away", "period")=="I saw the cat walk away."
+def test_add_punct():
+    assert sample.add_punct("Jump", "exclaimation")=="Jump!"
+    assert sample.add_punct("Did he go yet", "question")=="Did he go yet?"
+    assert sample.add_punct("I saw the cat walk away", "period")=="I saw the cat walk away."
