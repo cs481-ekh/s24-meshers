fab624516dc8b52bf83e62c184ef43613f931413
paulvxx <89420434+paulvxx@users.noreply.github.com>
2024-02-09 17:08:48 -0700

    Closes #32 (#37)

---
+++ pymgm/src/sample.py
@@ -0,0 +1,15 @@
+# python program to add punctuation to sentences
+
+def add_punct(sentence, punctuation):
+    if punctuation=="exclaimation":
+        sentence += "!"
+    elif punctuation=="period":
+        sentence += "."
+    elif punctuation=="question":
+        sentence += "?"
+    return sentence
+
+
+sentence = add_punct("Did you bake cake yesterday", "question")
+
+print(sentence)
---
+++ pymgm/test/test_sample.py
@@ -0,0 +1,8 @@
+import pytest
+from src import sample
+
+
+def test_add_punct():
+    assert sample.add_punct("Jump", "exclaimation")=="Jump!"
+    assert sample.add_punct("Did he go yet", "question")=="Did he go yet?"
+    assert sample.add_punct("I saw the cat walk away", "period")=="I saw the cat walk away."
