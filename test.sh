286af05ae8ab6365bfcaaa7a8ccf6f00f660b501
paulvxx <psv51037@gmail.com>
2024-02-10 17:44:12 -0700

    test script

--- test.sh
+++ test.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
-exit 127
+
+# Exit script on error
+set -e
+
+# Lint with flake8
+flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics
+flake8 . --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics
+
+# Set PYTHONPATH for pytest
+export PYTHONPATH=./src:$PYTHONPATH
+
+echo -e "Running tests ..."
+
+# Test with pytest
+pytest ./test
+
+echo -e "\rTests OK."
+
+exit 0
