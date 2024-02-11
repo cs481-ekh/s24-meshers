710d4cc883a95eaec9700b8de005fe47c7c61976
paulvxx <psv51037@gmail.com>
2024-02-10 18:43:22 -0700

    flexibility

--- build.sh
+++ build.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
+# Default python command is python3
+pyname="python3"
+
+# In case one wants to run python, py, ... instead of python3
+if [ ! -z "$1" ]; then
+    pyname="$1"
+fi
+
 # Exit script on error
 set -e
 
 echo "Building Python package using pyproject.toml..."
 
 # Ensure the build module is installed
-python3 -m pip install --upgrade build
+"$pyname" -m pip install --upgrade build
 
 # Build the package
-python3 -m build
+"$pyname" -m build
 
-echo "\rPackage built successfully."
+echo -e "\rPackage built successfully."
 
 exit 0
