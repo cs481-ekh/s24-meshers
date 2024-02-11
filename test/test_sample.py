93ba0501f0150563f78eb45f4ddf6a573bd67079
paulvxx <psv51037@gmail.com>
2024-02-10 17:55:10 -0700

    revert

--- test/test_sample.py
+++ test/test_sample.py
@@ -3,5 +3,5 @@ from src.pymgm_test import sample
 
 def test_add_punct():
     assert sample.add_punct("Jump", "exclaimation")=="Jump!"
-    assert sample.add_punct("Did he go yet", "exclaimation")=="Did he go yet?"
+    assert sample.add_punct("Did he go yet", "question")=="Did he go yet?"
     assert sample.add_punct("I saw the cat walk away", "period")=="I saw the cat walk away."
