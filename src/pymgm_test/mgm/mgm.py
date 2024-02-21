87d2dfdabeb177fbebd448cec716fc129e18ad13
seanbsu <seancalkins@u.boisestate.edu>
2024-02-21 11:19:07 -0700

    create mgm abstract class with unimplemented afun so testing class can use it

---
+++ src/pymgm_test/mgm/mgm.py
@@ -0,0 +1,10 @@
+from abc import ABC, abstractmethod
+
+class mgm(ABC):
+    def __init__(self):
+        # This is the constructor method. It's called when you create a new instance of the class.
+        # You can put any setup code here.
+        pass
+
+    def afun(self, mgmStruct, Lh):
+        raise NotImplementedError("Method not yet implemented")
