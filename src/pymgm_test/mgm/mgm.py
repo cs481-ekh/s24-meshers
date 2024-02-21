5bc12604d781b50a244314693ab57b472c417420
seanbsu <seancalkins@u.boisestate.edu>
2024-02-21 11:27:17 -0700

    add methods that abstract class will implement

--- src/pymgm_test/mgm/mgm.py
+++ src/pymgm_test/mgm/mgm.py
@@ -2,9 +2,16 @@ from abc import ABC, abstractmethod
 
 class mgm(ABC):
     def __init__(self):
-        # This is the constructor method. It's called when you create a new instance of the class.
-        # You can put any setup code here.
         pass
 
     def afun(self, mgmStruct, Lh):
+        raise NotImplementedError("Method not yet implemented")
+
+    def solve(self,mgmobj,fh,tol,accel,maxIters):
+        raise NotImplementedError("Method not yet implemented")
+
+    def multilevel(self,fh,multilevelStruct,smooths,uh):
+        raise NotImplementedError("Method not yet implemented")
+
+    def standalone(self,levelsData,fh,tol,max_iter,uh,smooths):
         raise NotImplementedError("Method not yet implemented")
