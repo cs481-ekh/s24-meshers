ab4c61aaf8123fad692162f56a2a8151b51f21e9
seanbsu <seancalkins@u.boisestate.edu>
2024-02-24 17:04:19 -0700

    broke up class functions into seperate files like in mgm, restrucutred first 2 tests to match what the function expects to be passed to it based on the Matlab implementation.

--- src/pymgm_test/mgm/mgm.py
+++ src/pymgm_test/mgm/mgm.py
@@ -1,17 +1,25 @@
-from abc import ABC, abstractmethod
+from abc import ABC, abstractmethod, abstractstaticmethod
+
 
 class mgm(ABC):
     def __init__(self):
+        self.levels_data = None
+        self.domain_vol = None
+        self.coarsening_factor = None
+        self.Nmin = 250
+        self.pre_smooth = 1
+        self.post_smooth = 1
+        self.max_iters = 100
+        self.has_const_nullspace = False
+
+    from ._afun import afun
+    def solve(self, mgmobj, fh, tol, accel, maxIters):
         pass
 
-    def afun(self, uh,mgmobj):
-        raise NotImplementedError("Method not yet implemented")
 
-    def solve(self,mgmobj,fh,tol,accel,maxIters):
-        raise NotImplementedError("Method not yet implemented")
+    def multilevel(self, fh, multilevelStruct, smooths, uh):
+        pass
 
-    def multilevel(self,fh,multilevelStruct,smooths,uh):
-        raise NotImplementedError("Method not yet implemented")
 
-    def standalone(self,levelsData,fh,tol,max_iter,uh,smooths):
-        raise NotImplementedError("Method not yet implemented")
+    def standalone(self, levelsData, fh, tol, max_iter, uh, smooths):
+        pass
