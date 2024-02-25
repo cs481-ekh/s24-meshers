bb7d4b6fe50f98cbd0b47b80c722b4a1e876d948
paulvxx <psv51037@gmail.com>
2024-02-24 21:47:09 -0700

    Created gallery class

---
+++ src/pymgm_test/utils/gallery.py
@@ -0,0 +1,34 @@
+import numpy as np
+from scipy.io import loadmat
+
+class Gallery_m:
+    # Constructor for processed information (Lh, x, fh, uexact)
+    # Default values set to None in case loading from a mat file (load_mat)
+    def __init__(self, Lh=None, x=None, fh=None, uexact=None):
+        self.Lh = Lh
+        self.x = x
+        self.fh = fh
+        self.uexact = uexact
+    
+    def __str__(self):
+        return "(" + str(self.Lh) + "," + str(self.x) + "," + str(self.fh) + "," + str(self.uexact) + ")"
+
+    def load_mat(self, filename):
+        mat_dict = loadmat(filename)
+        self.Lh = mat_dict['Lh']
+        self.x = mat_dict['x']
+        self.fh = mat_dict['fh']
+        self.uexact = mat_dict['uexact']
+
+    def _Lh__str__(self):
+        return str(self.Lh)
+
+    def _x__str__(self):
+        return str(self.x)
+
+    def _fh__str__(self):
+        return str(self.fh)
+
+    def _uexact__str__(self):
+        return str(self.uexact)
+
