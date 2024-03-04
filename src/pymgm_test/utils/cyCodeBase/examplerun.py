dd83a09c48c079b6863a2e8c659e628313529fe1
Tanner Frost <frost.tanner@live.com>
2024-03-04 16:33:20 -0700

    remove syspath test

--- src/pymgm_test/utils/cyCodeBase/examplerun.py
+++ src/pymgm_test/utils/cyCodeBase/examplerun.py
@@ -2,8 +2,6 @@
 
 # To compile 'elim' library: 
 # g++ -O3 -Wall -shared -std=c++11 -fPIC `python3 -m pybind11 --includes` bindings.cpp cySampleElim.h -o elim`python3-config --extension-suffix`  
-import sys
-print(sys.path)
 
 import elim
 
