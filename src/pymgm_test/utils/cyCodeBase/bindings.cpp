58c2105bfadcd8ad20b866ee05c23f70905b2c88
Tanner Frost <frost.tanner@live.com>
2024-03-04 16:31:35 -0700

    rearrange and add tests

--- src/pymgm_test/utils/cyCodeBase/bindings.cpp
+++ src/pymgm_test/utils/cyCodeBase/bindings.cpp
@@ -8,11 +8,6 @@ using namespace cy;
 PYBIND11_MODULE(elim, m) {
     py::class_<WeightedSampleElimination<cy::Point2d, double, 2, int>>(m, "WeightedSampleElimination")
       .def(py::init<>())
-      //.def("Eliminate", &WeightedSampleElimination<cy::Point2d, double, 2, int>::Eliminate)
-
-.def("Eliminate", [](const WeightedSampleElimination<float, float, 3> &self, const float *inputPoints, size_t inputSize, float *outputPoints, size_t outputSize, bool progressive, float d_max, int dimensions) {
-self.Eliminate(inputPoints, inputSize, outputPoints, outputSize, progressive, d_max, dimensions);
-}, py::arg("inputPoints"), py::arg("inputSize"), py::arg("outputPoints"), py::arg("outputSize"), py::arg("progressive")=false, py::arg("d_max")=0.0f, py::arg("dimensions")=3)
       .def("SetTiling", &WeightedSampleElimination<cy::Point2d, double, 2, int>::SetTiling, py::arg("on")=true)  // Bind the SetTiling function
       .def("IsTiling", &WeightedSampleElimination<cy::Point2d, double, 2, int>::IsTiling)
       .def("SetWeightLimiting", &WeightedSampleElimination<cy::Point2d, double, 2, int>::SetWeightLimiting, py::arg("on")=true)
--- src/pymgm_test/utils/cyCodeBase/examplerun.py
+++ src/pymgm_test/utils/cyCodeBase/examplerun.py
@@ -2,7 +2,8 @@
 
 # To compile 'elim' library: 
 # g++ -O3 -Wall -shared -std=c++11 -fPIC `python3 -m pybind11 --includes` bindings.cpp cySampleElim.h -o elim`python3-config --extension-suffix`  
-
+import sys
+print(sys.path)
 
 import elim
 
