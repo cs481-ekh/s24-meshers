a3ec323c9d90dcd00d83dec89ca960df93e37f01
seanbsu <122116208+seanbsu@users.noreply.github.com>
2024-03-08 08:03:39 -0700

    add ls to check if elim compiled

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ set -e
 # Compile 'elim' library
 
 g++ -O3 -Wall -shared -std=c++11 -fPIC `python3 -m pybind11 --includes` src/pymgm_test/utils/cyCodeBase/bindings.cpp src/pymgm_test/utils/cyCodeBase/cySampleElim.h -o elim`python3-config --extension-suffix`
-
+ls
 
 # Lint with flake8
 flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics
