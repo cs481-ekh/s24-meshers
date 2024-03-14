c10604baf05b4f3b5ce51ac2a53f5d934cb93bf6
Tanner Frost <frost.tanner@live.com>
2024-03-13 21:42:57 -0600

    Added back checkarguments handle in main Coarsen() function. updated data types

--- src/pymgm_test/utils/PcCoarsen2D.cpp
+++ src/pymgm_test/utils/PcCoarsen2D.cpp
@@ -2,13 +2,7 @@
  * Coarsens a 2D point cloud 
  * xc = PcCoarsen2D(x);
  * 
- * To create the mex file for the function
- * 1. Open matlab and change directories to where this file is saved
- * 2. Type "mex -I./cyCodeBase/ PcCoarsen2D.cpp" on the command line
- *    Change "-I./cyCodeBase/" to point to where you have the cyCodeBase package installed
  *
- * When compiling on AppleSilicon (M1 or M2), I found it necessary to include
- * the -DCY_NO_EMMINTRIN_H option with the mex compilation.
 */
 #include <iostream>
 #include <vector>
@@ -18,64 +12,63 @@
 #include "cyCodeBase/cyPoint.h"
 #include "cyCodeBase/cyHeap.h"
 
-
 using namespace cy;
 typedef unsigned int uint;
-int Nc = 4; 
-
-//using namespace std;
-// # define M_PI	3.14159265358979323846
 
 class PcCoarsen2D {
 public:
-// function + parameters
-    // return type of 
-	std::vector<std::array<double, 2>> Coarsen( vector<cy::Point2d> points, double area){
-    //void operator()(ArgumentList outputs, ArgumentList inputs) {
-        // checkArguments(outputs, inputs);
-        //uint Nc = (uint) inputs[1][0];
+    // points is input array.
+    // Want to coarsen to a vector array of (x,y) points coarsened to size outputPointCount
+    // Calls wse.eliminate() which loops and eliminates one point at a time until outputPointCount is reached
+    // return the vector array
+    std::vector <std::array<double, 2>> Coarsen(vector <cy::Point2d> points, int outputPointCount, float area) {
+      // check arguments
+      if (points.getType() != ArrayType::DOUBLE || points.getType() == ArrayType::COMPLEX_DOUBLE)
+      {
+        throw std::invalid_argument("Input points must be scalar double ");
+      }
 
-        //TypedArray<double> doubleArray = std::move(inputs[0]);
-        //int N = doubleArray.getNumberOfElements()/2;
-		int N = points.size();
-        std::vector< cy::Point2d > inputPoints(N);
-		// int count = 0;
- 		int idx = 0; 
-		for ( idx = 0; idx < N; idx++ ) {
-		    inputPoints[idx].x = points[idx][0];
-		    inputPoints[idx].y = points[idx][1];
-		}         
+      if (outputPointCount.getType() != ArrayType::DOUBLE)
+      {
+        throw std::invalid_argument("outputPointCount must be int ????? ");
+      }
 
-		// create weighted elimination object
-		cy::WeightedSampleElimination< Point2d, double, 2, int > wse;
+      TypedArray<double> doubleArray = std::move(points);
+      int N = doubleArray.getNumberOfElements()/2;
+      if (N <= outputPointCount) {
+        throw std::invalid_argument("Output size must be less than input to coarsen");
+      }
 
-		// execute weighted elimination
-		std::vector< cy::Point2d > outputPoints(Nc);
-        //float area = inputs[2][0];
-		float d_max = 2 * wse.GetMaxPoissonDiskRadius( 
-									2, outputPoints.size(), area );
-		
-		bool isProgressive = true;
-		wse.Eliminate( inputPoints.data(), inputPoints.size(), 
-		               outputPoints.data(), outputPoints.size(), 
-		               isProgressive, d_max, 2 );
+      std::vector <cy::Point2d> inputPoints(N);
+      int idx = 0;
+      for (idx = 0; idx < N; idx++) {
+        inputPoints[idx].x = doubleArray[idx][0];
+        inputPoints[idx].y = doubleArray[idx][1];
+      }
 
-		Nc = outputPoints.size();
+      // create weighted elimination object
+      cy::WeightedSampleElimination<Point2d, double, 2, int> wse;
 
-		//ArrayFactory f;
-		//TypedArray<double> xc = f.createArray<double>({Nc, 2});
-		
-		std::vector<std::array<double, 2>> xc(Nc);
-		
-		for ( idx = 0; idx < Nc; idx++ ) {
-		    xc[idx][0] = outputPoints[idx].x;
-		    xc[idx][1] = outputPoints[idx].y;
-		}        
-		//outputs[0] = xc;
-		return xc;
+      // execute weighted elimination
+      uint Nc = (uint) outputPointCount;
+      std::vector < cy::Point2d > outputPoints(Nc);
+
+      float d_max = 2 * wse.GetMaxPoissonDiskRadius(2, outputPoints.size(), area);
+
+      bool isProgressive = true;
+      wse.Eliminate(inputPoints.data(), inputPoints.size(),outputPoints.data(), outputPoints.size(), isProgressive, d_max, 2);
+
+      Nc = outputPoints.size();
+
+      std::vector <std::array<double, 2>> xc(Nc);
+
+      for (idx = 0; idx < Nc; idx++) {
+        xc[idx][0] = outputPoints[idx].x;
+        xc[idx][1] = outputPoints[idx].y;
+      }
+
+      return xc;
     }
-	
+
 
 };
-
-
