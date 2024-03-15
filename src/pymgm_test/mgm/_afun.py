4594bd6f1c55a72d46d20fccf67402893a8136f6
seanbsu <122116208+seanbsu@users.noreply.github.com>
2024-03-14 20:28:36 -0600

    Closes #52 (#76)

--- src/pymgm_test/mgm/_afun.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-
-
-def afun(self,uh, mgmobj):
-    Lh = mgmobj[4]['levelsData'][0]['Lh']
-    res = Lh.dot(uh)
-    return res # Assuming mgmobj is an array-like object with Lh attribute
