6e9ad6bac64914d9b1bed45af79743d3ca77dac7
Tanner Frost <frost.tanner@live.com>
2024-03-22 22:05:49 -0600

    Change dependencies and bindings. fix calling in runPc. Pc now links and calls

--- src/pymgm_test/utils/bindings.cpp
+++ src/pymgm_test/utils/bindings.cpp
@@ -8,6 +8,16 @@ PYBIND11_MODULE(PcCoarsen, m) {
     py::class_<PcCoarsen2D>(m, "PcCoarsen2D")
         .def(py::init<>())
         .def("Coarsen", &PcCoarsen2D::Coarsen);
+
+    py::class_<WeightedSampleElimination<cy::Point2d, double, 2, int>>(m, "WeightedSampleElimination")
+        .def(py::init<>())
+        .def("SetTiling", &WeightedSampleElimination<cy::Point2d, double, 2, int>::SetTiling, py::arg("on")=true)  // Bind the SetTiling function
+        .def("IsTiling", &WeightedSampleElimination<cy::Point2d, double, 2, int>::IsTiling)
+        .def("SetWeightLimiting", &WeightedSampleElimination<cy::Point2d, double, 2, int>::SetWeightLimiting, py::arg("on")=true)
+        .def("IsWeightLimiting", &WeightedSampleElimination<cy::Point2d, double, 2, int>::IsWeightLimiting)
+        .def("GetParamAlpha", &WeightedSampleElimination<cy::Point2d, double, 2, int>::GetParamAlpha)
+        .def("GetMaxPoissonDiskRadius", &WeightedSampleElimination<cy::Point2d, double, 2, int>::GetMaxPoissonDiskRadius,
+              py::arg("dimensions"), py::arg("sampleCount"), py::arg("domainSize")=0.0f);
 }
 
 
--- src/pymgm_test/utils/runPcCoarsen.py
+++ src/pymgm_test/utils/runPcCoarsen.py
@@ -13,25 +13,20 @@ def poisson_gen_base(n):
 
 N=8
 x = poisson_gen_base(N) #  generate 64 points
-
-
 vol=4
 
 # next level N, divide by coarsening factor
 NC = len(x) / 4
 
-# xc the next coarsened next level
-# xc is doubles
-# NC is double
-# vol is double
 # Create an instance of the PcCoarsen2D class
 PcCoarsen_2d = PcCoarsen.PcCoarsen2D()
 
 # Call the Coarsen method
+# xc the next coarsened next level
+# xc is doubles
+# NC is double
+# vol is double
 xc = PcCoarsen_2d.Coarsen(x,NC,vol)
-
-
-
 print("xc is:")
 print(xc)
 
