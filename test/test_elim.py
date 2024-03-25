5c3ec20bd4f28ab8b88b79429055a71812c074ce
Tanner Frost <frost.tanner@live.com>
2024-03-25 13:13:19 -0600

    updated elim for PcCoarsen module

--- test/test_elim.py
+++ test/test_elim.py
@@ -1,19 +1,23 @@
 import pytest
 import numpy as np
-from src.pymgm_test.utils.cyCodeBase import  elim
-wse = elim.WeightedSampleElimination()  # constructor with no arguments, build with defaults
+from src.pymgm_test.utils import PcCoarsen
+
+
+# Create an instance of the Elim class
+PcCoarsen_2d = PcCoarsen.PcCoarsen2D()
+wse = PcCoarsen.WeightedSampleElimination()  # constructor with no arguments, build with defaults
 
 
 def test_build_default_wse():
-    wse = elim.WeightedSampleElimination()  
+    wse = PcCoarsen.WeightedSampleElimination()  
     assert wse is not None
     
 def test_wse_is_tiling():
-    wse = elim.WeightedSampleElimination()  
+    wse = PcCoarsen.WeightedSampleElimination()  
     assert wse.IsTiling() == False
     
 def test_wse_set_tiling():
-    wse = elim.WeightedSampleElimination()  
+    wse = PcCoarsen.WeightedSampleElimination()  
     wse.SetTiling(True)
     assert wse.IsTiling() == True
     
@@ -21,16 +25,16 @@ def test_wse_is_limiting():
     assert wse.IsWeightLimiting() == True   
     
 def test_wse_set_limiting():
-    wse = elim.WeightedSampleElimination() 
+    wse = PcCoarsen.WeightedSampleElimination() 
     wse.SetWeightLimiting(False)
     assert wse.IsWeightLimiting() == False   
     
 def test_get_param_alpha():
-    wse = elim.WeightedSampleElimination() 
+    wse = PcCoarsen.WeightedSampleElimination() 
     assert wse.GetParamAlpha() == 8.0   
         
 def test_get_max_poisson_radius():
-    wse = elim.WeightedSampleElimination() 
+    wse = PcCoarsen.WeightedSampleElimination() 
     v=wse.GetMaxPoissonDiskRadius(2,16,0) 
     assert v == 0.1343212  
     
