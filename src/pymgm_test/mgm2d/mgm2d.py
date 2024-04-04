61efc4d9279285b92ad260596f1992cc20ac52f4
Tanner Frost <frost.tanner@live.com>
2024-04-04 08:56:42 -0600

    still working on my version. focused on simple demo and Coarsen calls

--- src/pymgm_test/mgm2d/mgm2d.py
+++ src/pymgm_test/mgm2d/mgm2d.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import math
+from scipy.spatial import cKDTree
 from src.pymgm_test.mgm.mgm import mgm
 from src.pymgm_test.utils import PcCoarsen
 
@@ -48,14 +49,39 @@ class mgm2D(mgm):
         plt.show()
         return h
 
+# Simple implementation for demo skips copies to locals, and skips range checking
     def constructor(self, Lh, x, domArea, hasConstNullspace, verbose):
-        # Actual construction here
-        #Compute number of levels
-        p = math.floor(math.log(N / Nmin) / math.log(coarseningFactor));
+        polyDim = (self.polyDegT+1)*(self.polyDegT+2)/2
+        minStencil = math.ceil(max(1.5*polyDim,3));  # Heuristic
+        computeDomArea = true
+        self.hasConstNullspace = false
+        verbose = false
+        rbfOrderT = 0
+        rbf = polyHarmonic
+        interpMethod = 0
+
+        N = x.shape[0] # get number of rows ( shape gives dimensions, then access row dim)
+        p = math.floor(math.log(N / self.Nmin) / math.log(self.coarseningFactor)) # #Compute number of levels
+
+        kdtree = cKDTree(x) # Build a cKDTree
+        distances, indices = kdtree.query(x, k=2) # Perform nearest neighbor search
+        domArea = x.shape[0] * np.mean(distances[:, 1])**2  # Compute domain area
+        self.domainVol = domArea
+
+        xc = np.empty(p + 1, dtype=object)
+        Nc = np.zeros(p + 1)
+        Nc[0] = N
+        xc[0] = x
+
+        #Build coarsened levels
+        for j in range(2, p + 2):
+            Nc[j] = math.floor(N / coarseningFactor ** (j - 1))
+            xc[j] = PcCoarsen2D(xc[j - 1], Nc[j], domArea)  # starting a j=2, this fills in the next level (first level keep same)
+            Nc[j] = xc[j].shape[0]
 
         levelsData = []
 
-        # Create level data for each level
+        # Initialize level data for each level
         for _ in range(p + 1):
             level_data = {
                 'nodes': [],
@@ -81,6 +107,9 @@ class mgm2D(mgm):
             }
             levelsData.append(level_data)
 
+        levelsData[1]['nodes'] = x
+        levelsData[0]['Lh'] = Lh
+        levelsData[0]['w'] = np.ones(N)
 
 
         obj = {}
