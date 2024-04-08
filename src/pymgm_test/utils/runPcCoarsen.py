cc6555f4c09a1b0bb3d988e910bc0e1c1b938dc0
Tanner Frost <frost.tanner@live.com>
2024-04-07 22:19:27 -0600

    in RunPcCoarsen Add more discrete test cases to track down anomalies.

--- src/pymgm_test/utils/runPcCoarsen.py
+++ src/pymgm_test/utils/runPcCoarsen.py
@@ -19,13 +19,18 @@ vol=4
 # next level N, divide by coarsening factor
 NC = len(x) / 4
 
-
+# base case is first level
 base_case_lvl_1 = x
 x1 = np.array([-0.77777778, -0.33333333, -0.77777778, -0.77777778, -0.11111111, -0.77777778, 0.11111111, 0.77777778, 0.77777778, -0.33333333, 0.33333333, -0.33333333, 0.11111111, 0.77777778, 0.33333333, 0.77777778])
-x2 = np.array([-0.77777778, -0.77777778, -0.33333333, 0.77777778, -0.33333333, 0.33333333, -0.77777778, -0.33333333, -0.77777778, 0.11111111, -0.11111111, 0.77777778, 0.33333333, 0.33333333, 0.77777778, 0.77777778])
+y1 = np.array([-0.77777778, -0.77777778, -0.33333333, 0.77777778, -0.33333333, 0.33333333, -0.77777778, -0.33333333, -0.77777778, 0.11111111, -0.11111111, 0.77777778, 0.33333333, 0.33333333, 0.77777778, 0.77777778])
+x3 = np.array([0.777777777777778,-0.777777777777778,-0.777777777777778,0.777777777777778])
+y3 = np.array([-0.777777777777778,0.777777777777778,-0.777777777777778,0.777777777777778])
 # expected coarsening in matlab with same input points
 # coarsening factor 4 on 64 => 16 points
-base_case_lvl_2 = np.column_stack((x1, x2))
+#expected level 2 after one coarsen
+base_case_lvl_2 = np.column_stack((x1, y2))
+#expected level 3 after one coarsen
+base_case_lvl_3 = np.column_stack((x3, y3))
 
 # Create an instance of the PcCoarsen2D class
 PcCoarsen_2d = PcCoarsen.PcCoarsen2D()
@@ -42,11 +47,36 @@ xc_reshaped = np.reshape(xc, (-1, 2))
 # Sort both arrays
 xc_sorted = np.sort(xc_reshaped, axis=1)
 xc_expect_sorted = np.sort(base_case_lvl_2, axis=1)
-print("xc sorted")
+print("xc sorted (lvl2)")
 print(xc_sorted)
 
-print("xc_expect_sorted")
+print("xc_expect_sorted (lvl2)")
 print(xc_expect_sorted)
 # Compare if sorted arrays are equal
-#are_equal = np.array_equal(xc_sorted, xc_expect_sorted)
-#print("Are arrays equal:", are_equal)
+are_equal = np.array_equal(xc_sorted, xc_expect_sorted)
+print("Are arrays equal:", are_equal)
+
+# Coarsen again (lvl 3)
+# expected level 3 after coarsen
+NC3 = len(base_case_lvl_2) / 4
+NC3_int = int(NC3) # coarsen from 16 to 4
+xc3 = PcCoarsen_2d.Coarsen(xc,NC3_int,vol_float)
+xc3_reshaped = np.reshape(xc3, (-1, 2))
+
+
+# Sort both arrays (lvl 3)
+xc3_sorted = np.sort(xc3_reshaped, axis=1)
+xc3_expect_sorted = np.sort(base_case_lvl_3, axis=1)
+print("xc sorted (lvl3)")
+print(xc3_sorted)
+
+print("xc3_expect_sorted (lvl3)")
+print(xc3_expect_sorted)
+# Compare if sorted arrays are equal
+are_equal3 = np.array_equal(xc3_sorted, xc3_expect_sorted)
+print("Are arrays equal:", are_equal3)
+
+# quick tests in runPcCorasen for now Tracking down anomalies
+# Test more discrete or larger cases
+# keyholepoissond, diskpoisson with variuus sizes
+# Then Build all this out to /test/test_PC_coarsen.py
