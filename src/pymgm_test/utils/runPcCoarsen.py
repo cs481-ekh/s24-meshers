96b3118a2ebabe19954f805119aba4fd82266a37
Tanner Frost <frost.tanner@live.com>
2024-04-22 04:48:27 -0600

    anomaly identified. test cases won't pass

--- src/pymgm_test/utils/runPcCoarsen.py
+++ src/pymgm_test/utils/runPcCoarsen.py
@@ -21,19 +21,17 @@ NC = len(x) / 4
 
 # base case is first level
 base_case_lvl_1 = x
-print("x is: ")
-print(x)
-print (" ")
+# print("x is: ")
+# print(x)
+# print (" ")
 x1 = np.array([-0.77777778, -0.33333333, -0.77777778, -0.77777778, -0.11111111, -0.77777778, 0.11111111, 0.77777778, 0.77777778, -0.33333333, 0.33333333, -0.33333333, 0.11111111, 0.77777778, 0.33333333, 0.77777778])
 y1 = np.array([-0.77777778, -0.77777778, -0.33333333, 0.77777778, -0.33333333, 0.33333333, -0.77777778, -0.33333333, -0.77777778, 0.11111111, -0.11111111, 0.77777778, 0.33333333, 0.33333333, 0.77777778, 0.77777778])
-x3 = np.array([0.777777777777778,-0.777777777777778,-0.777777777777778,0.777777777777778])
-y3 = np.array([-0.777777777777778,0.777777777777778,-0.777777777777778,0.777777777777778])
+
 # expected coarsening in matlab with same input points
 # coarsening factor 4 on 64 => 16 points
 #expected level 2 after one coarsen
 base_case_lvl_2 = np.column_stack((x1, y1))
-#expected level 3 after one coarsen
-base_case_lvl_3 = np.column_stack((x3, y3))
+
 
 # Create an instance of the PcCoarsen2D class
 PcCoarsen_2d = PcCoarsen.PcCoarsen2D()
@@ -44,64 +42,48 @@ PcCoarsen_2d = PcCoarsen.PcCoarsen2D()
 NC_int = int(NC) # coarsen from 64 to 16
 vol_float = float(vol)
 xc = PcCoarsen_2d.Coarsen(x,NC_int,vol_float)
-print("xc is: ")
-print(xc)
+# print("xc is: ")
+# print(xc)
 #xc_array = np.array(xc)
 # Transpose the array to swap rows and columns
 xy_column_vectors = np.reshape(xc, (-1, 2))
 
 xcx = xy_column_vectors[:, 0]  # First column (x)
 xcy = xy_column_vectors[:, 1]  # Second column (y)
-print("xcx is: ")
-print(xcx)
-print (" ")
+# print("xcx is: ")
+# print(xcx)
+# print (" ")
 
 xc_reshaped = np.column_stack((xcx, xcy))
 
-print("xc_reshaped is: ")
-print(xc_reshaped)
-print (" ")
+# print("xc_reshaped is: ")
+# print(xc_reshaped)
+# print (" ")
 
 
 # Sort both arrays
 
 
 xc_sorted2_indices = np.lexsort((xc_reshaped[:, 1], xc_reshaped[:, 0]))
-xc_sorted2 = xc_reshaped[xc_sorted2_indices]
-print("xc sorted2  lvl2")
-print(xc_sorted2)
+xc_sorted = xc_reshaped[xc_sorted2_indices]
+print("xc sorted (python)  lvl2")
+print(xc_sorted)
 #
-# xc_expect_sorted = base_case_lvl_2[base_case_lvl_2[:, 0].argsort()]
-# print("xc_expect_sorted (lvl2)")
-# print(xc_expect_sorted)
-# # Compare if sorted arrays are equal
-# are_equal = np.allclose(xc_sorted, xc_expect_sorted)
-# print("Are arrays equal (lvl2):", are_equal)
-#
-# # Coarsen again (lvl 3)
-# # expected level 3 after coarsen
-# NC3 = len(base_case_lvl_2) / 4
-# NC3_int = int(NC3) # coarsen from 16 to 4
-# xc3 = PcCoarsen_2d.Coarsen(xc,NC3_int,vol_float)
-# xc3x = xc3[:, 0]  # First column (x)
-# xc3y = xc3[:, 1]  # Second column (y)
-# xc3_reshaped = [xc3x, xc3y];
-# #xc3_reshaped = np.reshape(xc3, (-1, 2))
-#
-#
-# # Sort both arrays (lvl 3)
-# xc3_sorted = xc3_reshaped[xc3_reshaped[:, 0].argsort()]
-# xc3_expect_sorted = base_case_lvl_3[base_case_lvl_3[:, 0].argsort()]
-# print("xc sorted (lvl3)")
-# print(xc3_sorted)
-#
-# print("xc3_expect_sorted (lvl3)")
-# print(xc3_expect_sorted)
-# # Compare if sorted arrays are equal
-# are_equal3 = np.allclose(xc3_sorted, xc3_expect_sorted)
-# print("Are arrays equal (lvl3):", are_equal3)
-#
-# # quick tests in runPcCorasen for now Tracking down anomalies
-# # Test more discrete or larger cases
-# # keyholepoissond, diskpoisson with variuus sizes
-# # Then Build all this out to /test/test_PC_coarsen.py
+
+xc_lvl_2_expect_indices = np.lexsort((base_case_lvl_2[:, 1], base_case_lvl_2[:, 0]))
+xc_lvl_2_sorted = base_case_lvl_2[xc_lvl_2_expect_indices]
+print("xc_expect_sorted (matlab) (lvl2)")
+print(xc_lvl_2_sorted)
+tolerance = 1e-3  # Set your desired tolerance here
+
+# Check if the arrays are approximately equal with custom tolerance
+x_equal = np.allclose(xc_sorted[:,0], xc_lvl_2_sorted[:,0], atol=tolerance)
+y_equal = np.allclose(xc_sorted[:,1], xc_lvl_2_sorted[:,1], atol=tolerance)
+
+print("x_equal is: ",x_equal)
+
+
+#expected (matlab) level 3 after two coarsen
+# x3 = np.array([0.777777777777778,-0.777777777777778,-0.777777777777778,0.777777777777778])
+# y3 = np.array([-0.777777777777778,0.777777777777778,-0.777777777777778,0.777777777777778])
+# base_case_lvl_3 = np.column_stack((x3, y3))
